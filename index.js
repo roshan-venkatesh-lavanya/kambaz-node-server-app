80708ed0146b099cbadce7a2bdc2311cc8f19866
roshan-venkatesh-lavanya <roshan.venla@gmail.com>
2025-06-18 03:51:20 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-import express from 'express';
-import Hello from "./Hello.js";
+import express from "express";
 import Lab5 from "./Lab5/index.js";
-import cors from "cors";  
-import "dotenv/config";
-import session from "express-session";
+import Hello from "./Hello.js";
+import cors from "cors";
 import UserRoutes from "./Kambaz/Users/routes.js";
 import CourseRoutes from "./Kambaz/Courses/routes.js";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
-import EnrollmentRoutes from "./Kambaz/Enrollments/routes.js";
+import session from "express-session";
+import EnrollmentsRoutes from "./Kambaz/Enrollments/routes.js";
+import "dotenv/config";
+import mongoose from "mongoose";
 
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 
-app.use(
-    cors({
-   credentials: true,
-   origin: process.env.NETLIFY_URL || "http://localhost:5173",
-    })
-);  
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
 };
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
@@ -31,17 +30,22 @@ if (process.env.NODE_ENV !== "development") {
     domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
-app.use(session(sessionOptions));
-app.use(
-  session(sessionOptions)
-);
 
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || "http://localhost:5173",
+  })
+);
+app.use(session(sessionOptions));
 app.use(express.json());
+
 UserRoutes(app);
 CourseRoutes(app);
-ModuleRoutes(app);
-EnrollmentRoutes(app);
-AssignmentRoutes(app);
 Lab5(app);
-Hello(app)
-  app.listen(process.env.PORT || 4000)
+Hello(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+EnrollmentsRoutes(app);
+const port = process.env.PORT || 4000;
+app.listen(port);
